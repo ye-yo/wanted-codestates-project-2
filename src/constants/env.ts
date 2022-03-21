02cbccc6a3835396496b1778d155367f9b3f6ef7
ye-yo <yeyo0x0@gmail.com>
2022-03-22 00:20:58 +0900

    Feat: axios 활용 custom hook 및 useAPI 함수 구현
    Closes #5

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,1 +1,3 @@
 export const NEXON_TMI = `${process.env.REACT_APP_NEXON_TMI}`;
+export const NEXON_TMI_KEY = `${process.env.REACT_APP_NEXON_TMI_KEY}`;
+export const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
